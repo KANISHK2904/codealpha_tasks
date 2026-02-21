232093c6339ff9160f36904ec42fb8b83f835a4e
KANISHK2904 <kani29shk04@gmail.com>
2026-02-21 21:42:13 +0530

    creating hangmangame

--- CodeAlpha_hangmangame.py
+++ CodeAlpha_hangmangame.py
@@ -19,7 +19,7 @@ while attempts > 0:
     print("\nWord:", display)
 
     if "_" not in display:
-        print("🎉 You won!")
+        print("🎉 You won !")
         break
 
     guess = input("Enter a letter: ").lower()
